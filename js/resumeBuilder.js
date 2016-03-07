f2c41382707232c572f7a43344fffa4c90630a39
Greg Samek <gregsamek@users.noreply.github.com>
2016-03-06 22:20:24 -0500

    lesson 1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,64 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var work = {
+	"jobs": [
+	{
+		"employer": "Kimberly-Clark",
+		"title": "Scientist",
+		"location": "Atlanta",
+		"dates": "2012 - Present",
+		"description": "Corporate Research & Engineering"
+	}]
+};
+
+var projects = {
+	"projects": [
+	{
+		"title": "Vote App",
+		"dates": "2015",
+		"description": "Web application built with JQuery",
+		"images": ["images/voteApp.jpg"]
+	}]
+};
+
+var bio = {
+	"name": "Greg Samek",
+	"role": "Web Developer",
+	"welcomeMessage": "Thanks for stopping by! Check out my portfolio below.",
+	"contacts": {
+		"email": "Greg.Samek@gmail.com",
+		"twitter": "@GregSamek",
+		"github": "GregSamek",
+		"location": "Atlanta"
+	},
+	"skills": ["HTML", "CSS", "Javascript", "NodeJS"]
+};
+
+var education = {
+	"schools": [
+	{
+		"name": "University of Toledo",
+		"location": "Toledo, Ohio",
+		"degree": "Bachelor of Science",
+		"majors": ["Chemical Engineering"],
+		"dates": "2011",
+		"url": "utoledo.edu"
+	}],
+	"onlineCourses": [
+	{
+		"title": "Intro to HTML and CSS",
+		"school": "Udacity",
+		"dates": "2016",
+		"url": "https://www.udacity.com/courses/ud304"
+	},
+	{
+		"title": "Responsive Web Design",
+		"school": "Udacity",
+		"dates": "2016",
+		"url": "https://www.udacity.com/courses/ud893"
+	},
+	{
+		"title": "Responsive Images",
+		"school": "Udacity",
+		"dates": "2016",
+		"url": "https://www.udacity.com/courses/ud882"
+	}]
+};
